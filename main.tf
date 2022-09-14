6f8353d5f292957e8af2dc0869faabd586e45443
Alexander Giebelhaus <alex@GXH-Mac.fritz.box>
2022-09-14 19:56:18 +0200

    new source uri

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/gxhouse/s3-webapp/aws"
+  version = "1.0.0"
+  # insert required variables here
   name    = var.name
   region  = var.region
   prefix  = var.prefix
-  version = "1.0.0"
 }
